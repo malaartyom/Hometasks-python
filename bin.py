d43f40032446b34e0ec00babd1f5680cc2fc5cbd
malaartyom <malarenkoart@bk.ru>
2022-11-28 19:29:22 +0700

    Update mini_task10.py

--- bin.py
+++ bin.py
@@ -1,2 +1,6 @@
 a = {1: 17, 2: 15, 3:9, 4:21}
 print(min(a, key = lambda x: a[x]))
+if (1, 17) in a.items():
+    print(1)
+else:
+    print(0)
--- mini_task10.py
+++ mini_task10.py
@@ -7,10 +7,15 @@ class LRUCache:
     
     def put(self, key, value):
         if self.len < self.cap:
+            if key in self.data.keys():
+                self.call[key] += 1
+            else:
+                self.call[key] = 0
+                
             self.data[key] = value
-            self.call[key] = 0
         else:
             key_to_del = min(self.call, key=lambda x: self.call[x])
+            del self.data[key_to_del]
 
     def get(self, key):
         if (key, self.data[key]) in self.data.items():
